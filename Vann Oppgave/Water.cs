e06462a9b845b6d659d8053289f242a950da07e4
mrskme <eiriksb@getacademy.no>
2020-11-27 16:07:27 +0100

    Stuffs

--- Vann Oppgave/Water.cs
+++ Vann Oppgave/Water.cs
@@ -8,20 +8,28 @@ namespace Vann_Oppgave
     {
         public int Amount;
         public int Temperature;
+        public double ProportionFirstState;
         public WaterState State;
 
-        public Water(int amount, int temperature)
+        
+        public Water(int amount, int temperature, double proportionFirstState) :this (amount, temperature)
         {
-            if (temperature <= 0) State = WaterState.Ice;
+            ProportionFirstState = proportionFirstState;
+        }
+        public Water(int amount, int temperature) 
+        {
+            if (temperature < 0) State = WaterState.Ice;
+            else if (temperature == 0) State = WaterState.IceAndFluid;
             else if (temperature > 0 && temperature < 100) State = WaterState.Fluid;
-            else if (temperature >= 100) State = WaterState.Gas;
+            else if (temperature == 100) State = WaterState.FluidAndGas;
+            else if (temperature > 100) State = WaterState.Gas;
 
             Amount = amount;
             Temperature = temperature;
         }
         public enum WaterState
         {
-            Fluid, Ice, Gas,/* FluidAndGas, IceAndFluid*/
+            Fluid, Ice, Gas, FluidAndGas, IceAndFluid
         }
     }
 }
--- WaterTestLOL/UnitTest1.cs
+++ WaterTestLOL/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using Vann_Oppgave;
 
@@ -9,7 +10,6 @@ namespace WaterTestLOL
         public void Setup()
         {
         }
-
         [Test]
         public void Test1()
         {
@@ -31,6 +31,27 @@ namespace WaterTestLOL
             var water = new Water(50, 120);
             Assert.AreEqual(Water.WaterState.Gas, water.State);
             Assert.AreEqual(120, water.Temperature);
+        }
+        //[Test]
+        //// Ved 0 og 100 grader må vi angi en frivillig parameter til constructoren som angir hvor stor del 
+        //// som er i den første fasen (altså is ved blanding av is og flytende - og flytende ved blanding 
+        //// av flytende og gass). Denne testen sjekker at vi får exception om dette ikke er angitt og temperaturen
+        //// er 100 grader. 
+        //[ExpectedException(typeof(ArgumentException),
+        //    "When temperature is 0 or 100, you must provide a value for proportion")]
+        //public void Test04WaterAt100DegreesWithoutProportion()
+        //{
+        //    var water = new Water(50, 100);
+        //}
+
+        [Test]
+        // Sjekker at vi får miks av flytende og gass, med 30% av det første
+        public void Test05WaterAt100Degrees()
+        {
+            var water = new Water(50, 100, 0.3);
+            Assert.AreEqual(Water.WaterState.FluidAndGas, water.State);
+            Assert.AreEqual(100, water.Temperature);
+            Assert.AreEqual(0.3, water.ProportionFirstState);
         }
     }
 }
